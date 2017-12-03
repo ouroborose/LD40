0a2938558b560791f148ed6f8276d31b31856c26
stregawolf <stregawolf@gmail.com>
2017-12-02 17:33:36 -0800

    added place holder rash

--- Assets/LD40/Scripts/Main.cs
+++ Assets/LD40/Scripts/Main.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Main : Singleton<Main> {
+    public static RaycastHit2D[] s_raycastHits = new RaycastHit2D[100];
+
     public CameraManager m_cameraManager;
     public Hand m_hand;
 
-    public int m_playAreaWidth = 100;
-    public int m_playAreaHeight = 100;
+    public GameObject m_rashPrefab;
 
+    public List<Rash> m_rashes = new List<Rash>();
 
     protected Plane m_interactionPlane;
 
@@ -38,6 +40,28 @@ public class Main : Singleton<Main> {
         if (Input.GetMouseButtonDown(0))
         {
             m_hand.Scratch();
+            int hitCount = Physics2D.RaycastNonAlloc(m_hand.transform.position, Vector2.zero, s_raycastHits);
+            int rashHits = 0;
+            for (int i = 0; i < hitCount; ++i)
+            {
+                RaycastHit2D hit = s_raycastHits[i];
+                Rash rash = hit.collider.GetComponentInParent<Rash>();
+                if(rash != null)
+                {
+                    rashHits++;
+                    rash.Grow();
+                }
+            }
+
+            if(rashHits <= 0 && hitCount > 0)
+            {
+                // spawn new rash here
+                GameObject rashObj = Instantiate(m_rashPrefab, transform);
+                rashObj.transform.position = mouseRay.GetPoint(d);
+                rashObj.transform.Rotate(Vector3.forward, Random.value * 360.0f);
+                Rash newRash = rashObj.GetComponent<Rash>();
+                m_rashes.Add(newRash);
+            }
         }
     }
 }
---
+++ Assets/LD40/Scripts/Objects/Rash.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rash : MonoBehaviour {
+    public const float RASH_GROWTH_AMOUNT = 0.1f;
+    public void Grow()
+    {
+        transform.localScale += Vector3.one * RASH_GROWTH_AMOUNT;
+    }
+}
