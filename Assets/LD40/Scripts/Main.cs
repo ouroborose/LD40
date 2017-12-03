0ae3c45fece5cffae9f91d419258c2ec101f0b7c
stregawolf <stregawolf@gmail.com>
2017-12-02 22:14:48 -0800

    added rash spreading logic

--- Assets/LD40/Scripts/Main.cs
+++ Assets/LD40/Scripts/Main.cs
@@ -13,12 +13,15 @@ public class Main : Singleton<Main> {
     public List<Rash> m_rashes = new List<Rash>();
 
     protected Plane m_interactionPlane;
+    protected List<Rash> m_scratchedThisFrame = new List<Rash>();
+    protected List<Rash> m_itchPoints = new List<Rash>();
 
     override protected void Awake()
     {
         base.Awake();
 
         m_interactionPlane = new Plane(Vector3.back, 0);
+        m_itchPoints.AddRange(m_rashes);
     }
 
     protected void Update()
@@ -49,19 +52,137 @@ public class Main : Singleton<Main> {
                 if(rash != null)
                 {
                     rashHits++;
-                    rash.Grow();
+                    Scratch(rash);
+
+                    if(m_itchPoints.Contains(rash))
+                    {
+                        // give sanity
+                    }
+                    else
+                    {
+                        // give itch point hint
+                    }
                 }
             }
 
             if(rashHits <= 0 && hitCount > 0)
             {
                 // spawn new rash here
-                GameObject rashObj = Instantiate(m_rashPrefab, transform);
-                rashObj.transform.position = mouseRay.GetPoint(d);
-                rashObj.transform.Rotate(Vector3.forward, Random.value * 360.0f);
-                Rash newRash = rashObj.GetComponent<Rash>();
-                m_rashes.Add(newRash);
+                SpawnRash(mouseRay.GetPoint(d));
+            }
+
+        }
+
+        // clear stratch state
+        for (int i = 0; i < m_scratchedThisFrame.Count; ++i)
+        {
+            m_scratchedThisFrame[i].m_scratchedThisFrame = false;
+        }
+        m_scratchedThisFrame.Clear();
+    }
+
+    public void Scratch(Rash rash)
+    {
+        if(rash.m_scratchedThisFrame)
+        {
+            return;
+        }
+
+        rash.m_scratchedThisFrame = true;
+        rash.Scratch();
+        m_scratchedThisFrame.Add(rash);
+    }
+
+    public void TryToSpreadRash(Rash source, float minRadius, float maxRadius)
+    {
+        Vector2 center = source.transform.position;
+        int attempts = 0;
+        while(attempts < 50)
+        {
+            Vector2 spawnPos = center + Random.insideUnitCircle.normalized * Random.Range(minRadius, maxRadius);
+            int hits = Physics2D.RaycastNonAlloc(spawnPos, Vector2.zero, s_raycastHits);
+            if(hits > 0)
+            {
+                int rashHits = 0;
+                for (int i = 0; i < hits; ++i)
+                {
+                    RaycastHit2D hit = s_raycastHits[i];
+                    Rash rash = hit.collider.GetComponentInParent<Rash>();
+                    if (rash != null)
+                    {
+                        rashHits++;
+                        Scratch(rash);
+                    }
+                }
+
+                if (rashHits <= 0)
+                {
+                    // hit skin so spawn a new rash
+                    SpawnRash(spawnPos); 
+                }
+                return;
+            }
+            
+            attempts++;
+        }
+    }
+
+    public void SpawnRash(Vector3 pos)
+    {
+        GameObject rashObj = Instantiate(m_rashPrefab, transform);
+        rashObj.transform.position = pos;
+        rashObj.transform.Rotate(Vector3.forward, Random.value * 360.0f);
+        Rash newRash = rashObj.GetComponent<Rash>();
+        m_rashes.Add(newRash);
+    }
+
+    public void GiveHint(Vector3 pos)
+    {
+        Rash closestItch = FindClosestItch(pos);
+        if(closestItch != null)
+        {
+            // give hint
+            Vector3 delta = closestItch.transform.position - pos;
+            if(Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+            {
+                if(delta.x < 0)
+                {
+                    // hint left
+                }
+                else
+                {
+                    // hint right
+                }
+            }
+            else
+            {
+                if (delta.y < 0)
+                {
+                    // hint down
+                }
+                else
+                {
+                    // hint up
+                }
             }
         }
+    }
+
+    public Rash FindClosestItch(Vector3 pos)
+    {
+        Rash closestItch = null;
+        float closestDist = float.MaxValue;
+        for (int i = 0; i < m_itchPoints.Count; ++i)
+        {
+            Rash rash = m_itchPoints[i];
+            float dist = Vector3.SqrMagnitude(rash.transform.position - pos);
+            if(dist < closestDist)
+            {
+                closestItch = rash;
+                closestDist = dist;
+            }
+        }
+
+        return closestItch;
     }
 }
--- Assets/LD40/Scripts/Objects/Rash.cs
+++ Assets/LD40/Scripts/Objects/Rash.cs
@@ -2,10 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rash : MonoBehaviour {
+public class Rash : MonoBehaviour
+{
+    public const int MIN_SPREAD_SCRATCHES = 1;
+    public const int MAX_SPREAD_SCRATCHES = 1;
     public const float RASH_GROWTH_AMOUNT = 0.1f;
-    public void Grow()
+    public const float MAX_SIZE = 2.0f;
+
+    public bool m_scratchedThisFrame = false;
+
+    public SpriteRenderer[] m_sprites;
+
+    protected int m_overScratchCount = 0;
+    protected int m_nextSpreadCountRequirement = 0;
+
+    protected float m_currentSize = 1.0f;
+
+    protected void Awake()
     {
-        transform.localScale += Vector3.one * RASH_GROWTH_AMOUNT;
+        m_sprites = GetComponentsInChildren<SpriteRenderer>();
+        RandomizeNextSpreadRequirement();
+    }
+
+    public void RandomizeNextSpreadRequirement()
+    {
+        m_nextSpreadCountRequirement = Random.Range(MIN_SPREAD_SCRATCHES, MAX_SPREAD_SCRATCHES);
+    }
+
+    public void Scratch()
+    {
+        if(TryToGrow())
+        {
+            m_overScratchCount++;
+            if(m_overScratchCount > m_nextSpreadCountRequirement)
+            {
+                Bounds bounds = GetBounds();
+                Main.Instance.TryToSpreadRash(this, bounds.extents.x*0.75f, bounds.extents.x * 1.25f);
+                RandomizeNextSpreadRequirement();
+            }
+        }
+    }
+
+    public bool TryToGrow()
+    {
+        m_currentSize += RASH_GROWTH_AMOUNT;
+        bool isMaxSize = m_currentSize >= MAX_SIZE;
+        if (isMaxSize)
+        {
+            m_currentSize = MAX_SIZE;
+        }
+        SetAlpha(0.25f + 0.5f*m_currentSize/MAX_SIZE);
+        transform.localScale = Vector3.one * m_currentSize;
+        return isMaxSize;
+    }
+
+    public void SetAlpha(float alpha)
+    {
+        for(int i = 0; i < m_sprites.Length; ++i)
+        {
+            Color c = m_sprites[i].color;
+            c.a = alpha;
+            m_sprites[i].color = c;
+        }
+    }
+
+    public Bounds GetBounds()
+    {
+        Bounds bounds = new Bounds(transform.position, Vector3.zero);
+        for (int i = 0; i < m_sprites.Length; ++i)
+        {
+            bounds.Encapsulate(m_sprites[i].bounds);
+        }
+        return bounds;
     }
 }
